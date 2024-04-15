89e7a4eb535512f6163d82984e089a9f73364d2b
Andrew <andrew.t.messina@gmail.com>
2024-04-14 21:50:31 -0700

    Added comments to python files

--- Starter_Code/PyBank/Resources/main.py
+++ Starter_Code/PyBank/Resources/main.py
@@ -1,21 +1,29 @@
+#import csv modules
 import os
 import csv
 
+#set path for file
 csvpath = os.path.join('..', 'Resources','budget_data.csv')
 
+#open csv file and skip header row
 with open(csvpath) as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csv_reader)
+    #setting variables to 0 for iteration counting/summing
     mon_count = 0
     pro_loss_sum = 0
     pl_diff_sum = 0
     prev_row_delta = 0
+    #set max and min values to large numbers to return max/min later
     largest_inc = int(-1000000000000)
     largest_dec = int(1000000000000)
 
+#loop through csv file
     for r in csv_reader:
+        #counter to count number of rows, adding sum to profit/loss column
         mon_count = mon_count + 1
         pro_loss_sum = pro_loss_sum + int(r[1])
+        #Skip row 1 of data to calculate change in profit/loss for each row in data and sum it
         if mon_count > 1:
             pl_diff_row = int(r[1]) - prev_row_delta
             pl_diff_sum = pl_diff_row + pl_diff_sum
@@ -26,9 +34,9 @@ with open(csvpath) as csvfile:
                 largest_dec = pl_diff_row
                 dec_date = r[0]
         prev_row_delta = int(r[1])
-
+#Take average of calculated sum of all profit/loss changes and round to 2 decimal places
     avg_change = round(pl_diff_sum / (mon_count-1) ,ndigits= 2)
-    
+ #print out the results
     print("Financial Analysis")
     print("--------------------------------")
     print(f'Total Months: {mon_count}')
@@ -36,7 +44,3 @@ with open(csvpath) as csvfile:
     print(f'Average Change: ${avg_change}')
     print(f'Greatest Increase in Profits: {inc_date} (${largest_inc})')
     print(f'Greatest Decrease in Profits: {dec_date} (${largest_dec})')
-
-
-    
-        
--- Starter_Code/PyPoll/Resources/main.py
+++ Starter_Code/PyPoll/Resources/main.py
@@ -1,11 +1,14 @@
+#import csv modules
 import os
 import csv
 
+#set path for csv fle
 csvpath = os.path.join('..', 'Resources','election_data.csv')
-
+#open csv file and skip header row
 with open(csvpath) as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csv_reader)
+    #set variables for candidates and vote counts
     CCS = "Charles Casper Stockham"
     DD = "Diana DeGette"
     RAD = "Raymon Anthony Doane"
@@ -13,23 +16,28 @@ with open(csvpath) as csvfile:
     CCS_votes = 0
     DD_votes = 0
     RAD_votes = 0
+    #loop through csv file and count the number of rows(total_votes)
     for r in csv_reader:
         tot_votes = tot_votes + 1
+        #if statements that count # of votes for each candidate if column reads that specific candidate
         if r[2] == CCS:
             CCS_votes = CCS_votes + 1
         elif r[2] == DD:
             DD_votes = DD_votes + 1
         elif r[2] == RAD:
-            RAD_votes = RAD_votes + 1 
+            RAD_votes = RAD_votes + 1
+            #if statements to find out winner based on highest vote total 
             if CCS_votes > DD_votes and RAD_votes:
                 winner = CCS
             elif DD_votes > CCS_votes and RAD_votes:
                 winner = DD
             elif RAD_votes > DD_votes and CCS_votes:
                 winner = RAD 
+    #create variables to calculate percent votes for each candidate of the total           
     CCS_percent = round((CCS_votes / tot_votes) * 100, ndigits=3)
     DD_percent = round((DD_votes / tot_votes) * 100, ndigits=3)
-    RAD_percent = round((RAD_votes / tot_votes) * 100, ndigits=3)   
+    RAD_percent = round((RAD_votes / tot_votes) * 100, ndigits=3) 
+    #print results  
     print("Election Results")
     print("-------------------------")
     print(f'Total Votes: {tot_votes}')
